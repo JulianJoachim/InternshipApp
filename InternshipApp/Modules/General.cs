71bc830eb93ca68ad32f6eb5affa80c19df6fbef
JulianJoachim <JulianJoachim@JMCJ57KHR2>
2021-12-01 13:18:35 +0100

    Add project files.

---
+++ InternshipApp/Modules/General.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipApp.Modules
+{
+    public class General : ModuleBase<SocketCommandContext>
+
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("Pong!");
+        }
+
+    }
+}
